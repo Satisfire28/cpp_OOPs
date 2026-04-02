6c127da14f21ec6729ef7bbbe845f8f04e0587f8
Vishal Lodhi <lodhivishal286@gmail.com>
2026-04-02 11:56:50 +0530

    added readme.md

--- lecture1.cpp
+++ lecture1.cpp
@@ -1,18 +1,56 @@
 #include<iostream>
 using namespace std;
+class a{
+    public: 
+    int b;
+    char c,d;
+};
 class Student
 {
-    public: 
+    private:  
     string name;
     int age,rollno;
     string grade;
+   //function getter and setter 
+   public: 
+   void setname(string s){
+    name=s;
+   }
+   void setage(int a){
+    age=a;
+   }
+    void setrollno(int r){
+     rollno=r;
+    }
+    void setgrade(string g){
+     grade=g;
+    }
+    void getname(){
+        cout<<name;
+    }
+    int getrollno(){
+        return rollno;
+    }
+    string getgrade(int pin){
+        if(pin==123){
+            return grade;
+        }
+        else{
+            return "invalid pin";
+        }
+    }
 };
 int main()
 {
     Student s1;
-    s1.name="vishal lodhi";
-    s1.age=22;
-    s1.rollno=23;
-    s1.grade="A";
-    cout<<s1.name<<" "<<s1.age<<" "<<s1.rollno<<" "<<s1.grade<<endl;
+    s1.setname("vishal lodhi");
+    s1.setage(22);
+    s1.setrollno(23);
+    s1.setgrade("A");
+   s1.getname();
+   cout<<s1.getrollno();
+    cout<<s1.getgrade(123);
+    a obj;
+    cout<<sizeof(obj);
+    return 0;
 }
---
+++ readme.md
@@ -0,0 +1,1 @@
+lecture 1 contains : getter setter, paddding , dynamic memory allocation , classes and objects, greedy alignments. 
