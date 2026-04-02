1bd13eed07980d904cb758b517dba1b1468420c3
Vishal Lodhi <lodhivishal286@gmail.com>
2026-04-02 11:06:16 +0530

    created a class student

--- lecture1.cpp
+++ lecture1.cpp
@@ -1,8 +1,16 @@
 #include<bits/stdio.h>
+using namespace std;
+class Student{
+    string name;
+    int age,rollno;
+    string grade;
+}
 int main()
 {
-    int a,b;
-    scanf("%d%d",&a,&b);
-    printf("%d",a+b);
-    return 0;
+    student s1;
+    s1.name="vishal lodhi";
+    s1.age=22;
+    s1.rollno=23;
+    s1.grade="A"
+    cout<<s1.name<<" "<<s1.age<<" "<<s1.rollno<<" "<<s1.grade<<endl;
 }
