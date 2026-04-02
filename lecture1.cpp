17c51c131db39a3dde5516bf6fde39793697c52b
Vishal Lodhi <lodhivishal286@gmail.com>
2026-04-02 11:10:32 +0530

    removed the syntax error

--- lecture1.cpp
+++ lecture1.cpp
@@ -1,16 +1,18 @@
-#include<bits/stdio.h>
+#include<iostream>
 using namespace std;
-class Student{
+class Student
+{
+    public: 
     string name;
     int age,rollno;
     string grade;
-}
+};
 int main()
 {
-    student s1;
+    Student s1;
     s1.name="vishal lodhi";
     s1.age=22;
     s1.rollno=23;
-    s1.grade="A"
+    s1.grade="A";
     cout<<s1.name<<" "<<s1.age<<" "<<s1.rollno<<" "<<s1.grade<<endl;
 }
