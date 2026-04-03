285873733dc9a7908a57d17be5920c9240900f49
Vishal Lodhi <lodhivishal286@gmail.com>
2026-04-03 12:37:04 +0530

    started day2

--- lecture2.cpp
+++ lecture2.cpp
@@ -6,13 +6,24 @@ class customer{
     string name;
     int account_number;
     int balance;
+    //default constructor
     customer(){
         cout<<"constructor is called";
 
     };
+    //parameterized constructor
+    customer(string a,int b, int c){{
+        name=a;
+        account_number=b;
+        balance=c;
+    }
+
+    }
     
     };
     int main(){
         customer c1;
+        customer c2("vishal",123,1000);
+        cout<<c2.name<<" "<<c2.account_number<<" "<<c2.balance;
         return 0;
 };
