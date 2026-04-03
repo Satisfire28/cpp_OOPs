2d7f0e99c72aede5f96477d31d57cfce70b1c67a
Vishal Lodhi <lodhivishal286@gmail.com>
2026-04-03 23:02:52 +0530

    updated different cunstructors

--- lecture2.cpp
+++ lecture2.cpp
@@ -9,21 +9,32 @@ class customer{
     //default constructor
     customer(){
         cout<<"constructor is called";
+        name="vishal";
+        account_number=123;
+        balance=1000;
 
     };
-    //parameterized constructor
-    customer(string a,int b, int c){{
-        name=a;
-        account_number=b;
-        balance=c;
-    }
+    //parameterized constructor //custurctor overloading
+    // customer(string name, int account_number, int balance){
+    //     this->name = name;
+    //     this->account_number = account_number;
+    //     this->balance = balance;
+    // };
+    //inline custoructor
 
-    }
-    
+    inline customer(string a, int b, int c): name(a), account_number(b), balance(c){};
+
+    void display(){
+        cout<<name<<" "<<account_number<<" "<<balance<<endl;
+    };
+
     };
     int main(){
-        customer c1;
-        customer c2("vishal",123,1000);
-        cout<<c2.name<<" "<<c2.account_number<<" "<<c2.balance;
+        customer a1,a2,a3;
+        a1.display();
+        customer c3("aman",234,23423);
+        customer c2("vishal lodhi",123,1000);
+        c2.display();
+        c3.display();
         return 0;
 };
